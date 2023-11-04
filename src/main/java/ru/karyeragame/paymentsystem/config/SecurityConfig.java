bc9fa52aef7bdc9d5bc315e97076e1e09bb1301a
Vladimir Savitskiy <vladsav33@yandex.ru>
2023-11-04 16:29:04 +0300

    hotfix: changed SecurityConfig

--- src/main/java/ru/karyeragame/paymentsystem/config/SecurityConfig.java
+++ src/main/java/ru/karyeragame/paymentsystem/config/SecurityConfig.java
@@ -4,9 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
@@ -26,16 +23,6 @@ public class SecurityConfig {
                 .anyRequest().authenticated())
                 .httpBasic(withDefaults());
         return httpSecurity.build();
-    }
-
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("test")
-                .roles("TEST")
-                .build();
-        return new InMemoryUserDetailsManager(user);
     }
 }
 
